dc49e8379fbfdfb79661903ee7cf7aa0234b5f8b
Joel Anthony <janthony0328@gmail.com>
2025-11-14 21:58:02 -0500

    Finished Full Adder w/ Vivado Implementation
    Also added config file for Basys 3

---
+++ Basys3/Full_Adder_Basys3/Full_Adder_Basys3.srcs/sources_1/new/fulladd.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+module fulladd(sum, c_out, a, b, c_in);
+output sum, c_out;
+input a, b, c_in;
+wire s1, c1, c2;
+xor (s1, a, b);
+and (c1, a, b);
+xor (sum, s1, c_in);
+and (c2, s1, c_in);
+or(c_out, c2, c1);
+endmodule
---
+++ Basys3/Full_Adder_Basys3/Full_Adder_Basys3.srcs/sources_1/new/fulladd4.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/14/2025 09:11:55 PM
+// Design Name: 
+// Module Name: fulladd4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fulladd4(sum, c_out, a, b, c_in);
+output [3:0] sum; 
+output c_out;
+input [3:0] a, b; 
+input c_in;
+wire c1, c2, c3;
+
+fulladd fa0(sum[0], c1, a[0], b[0], c_in);
+fulladd fa1(sum[1], c2, a[1], b[1], c1);
+fulladd fa2(sum[2], c3, a[2], b[2], c2);
+fulladd fa3(sum[3], c_out, a[3], b[3], c3);
+endmodule
+
+module fulladd(sum, c_out, a, b, c_in);
+output sum, c_out;
+input a, b, c_in;
+wire s1, c1, c2;
+xor (s1, a, b);
+and (c1, a, b);
+xor (sum, s1, c_in);
+and (c2, s1, c_in);
+or(c_out, c2, c1);
+endmodule
